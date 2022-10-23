4c1367b308eeb5accd7aa8b4beed8361f289d75b
JagdpantherK <jagpanthertitanium@gmail.com>
2022-10-23 19:32:22 +0600

    Totally changed design and added clicker

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const button = document.querySelector("#button-two");
 const score = document.querySelector("#score");
-const divBlock = document.querySelector("#imgInsert");
 let clicks = 0;
 let image = document.querySelector("#none")
 image.classList.add('hide')
@@ -8,15 +7,12 @@ button.addEventListener("click", (where, element) => {
     image.classList.add('hide')
     clicks += 1;
     score.innerHTML = clicks;
-    if(clicks%5==0){
+    if(clicks%2===0){
         let color = "#";
         color += Math.random().toString(16).slice(2,8);
         button.style.background = color;
     }
-    if(clicks==5 || clicks==10 || clicks==50 || clicks==100){
+    if(clicks===5 || clicks===10 || clicks===50 || clicks===100){
         image.classList.remove('hide');
     }
-    // if(clicks%5!=0) {
-    //     none_img.parentNode.removeChild(none_img)
-    // }
 })
