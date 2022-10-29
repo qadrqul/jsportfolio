ab3eac2637e2db0b2c5db27c8059ce89bc6683b9
JagdpantherK <jagpanthertitanium@gmail.com>
2022-10-29 12:39:43 +0600

    added random image api and connected with my clicker

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const button = document.querySelector("#button-two");
 const score = document.querySelector("#score");
+const catImage = document.getElementById('cat-img');
 let clicks = 0;
-let image = document.querySelector("#none")
-image.classList.add('hideImg')
+fetch('https://aws.random.cat/meow')
+    .then(response => response.json())
+    .then(data => {
+        catImage.innerHTML = `<img src="${data.file}"/>`;
+    })
 button.addEventListener("click", (where, element) => {
-    image.classList.add('hideImg')
     clicks += 1;
     score.innerHTML = clicks;
     if(clicks%2===0){
@@ -12,7 +15,9 @@ button.addEventListener("click", (where, element) => {
         color += Math.random().toString(16).slice(2,8);
         button.style.background = color;
     }
-    if(clicks===5 || clicks===10 || clicks===50 || clicks===100){
-        image.classList.remove('hideImg');
-    }
+    fetch('https://aws.random.cat/meow')
+        .then(response => response.json())
+        .then(data => {
+            catImage.innerHTML = `<img src="${data.file}"/>`;
+        })
 })
